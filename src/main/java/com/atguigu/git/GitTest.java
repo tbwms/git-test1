44126c9a0a0aafff493078cc4d8c837a33ee606f
Tb <123@qq.com>
2022-06-12 12:20:46 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
