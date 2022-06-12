9c4e6e03dfe5dce4358b70b6d44681c2b48f907d
Tb <123@qq.com>
2022-06-12 12:18:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @title: GitTest
+ * @projectName: git-test
+ * @author: 田斌
+ * @date: 2022/6/12 12:15
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
